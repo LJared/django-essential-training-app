c8aba64618e5b50b88536ba7f2336589ed6ec2e7
Jared García <ljared.31g@gmail.com>
2024-06-22 13:07:19 -0600

    feat(Note): added a template to render all the notes stored in database and a view to show receive the request

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
+def list(request):
+    """
+    Get all notes and render them.
+    """
+    # Get all notes from the database.
+    all_notes = Note.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
