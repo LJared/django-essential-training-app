53b965bf9bb94f4151aeb5f3f5dbd04816304df0
Jared García <ljared.31g@gmail.com>
2024-06-22 11:38:34 -0600

    feat(home): added a new view and templated that are protected against not logged in users

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 def home(request):
@@ -7,3 +8,9 @@ def home(request):
     return render(request, "home/welcome.html", {
         'today': datetime.today(),
     })
+
+
+@login_required(login_url='/admin')
+def authorized(request):
+    """Handles requests to the authorized page."""
+    return render(request, "home/authorized.html", {})
