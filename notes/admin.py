6998ec840c6d2710fd3d61387e3bbd1e522c4492
Jared García <ljared.31g@gmail.com>
2024-06-22 12:25:35 -0600

    feat(Note): renamed Notes model to Note and registered on the admin site

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    # This is a list of fields that will be displayed
+    # in the admin site as table columns of the Note model.
+    list_display = ('title', 'text', 'created_at',)
+
+admin.site.register(models.Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
