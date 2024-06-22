b0a63cd106b7153c7f9cc0f7e5d9735914ad66c1
Jared García <ljared.31g@gmail.com>
2024-06-22 13:40:04 -0600

    refactoring(Home/views): using class-based views instead of function-based views

--- home/views.py
+++ home/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime
 
-def home(request):
-    """Handles requests to the home page."""
-    return render(request, "home/welcome.html", {
-        'today': datetime.today(),
-    })
+
+class HomeView(TemplateView):
+    template_name = "home/welcome.html"
+    extra_context = {'today': datetime.today()}
 
 
-@login_required(login_url='/admin')
-def authorized(request):
-    """Handles requests to the authorized page."""
-    return render(request, "home/authorized.html", {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = "home/authorized.html"
+    login_url = '/admin'
