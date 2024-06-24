1bc58c3bac8e61661d74ba48befb3463d298ba35
Jared García <ljared.31g@gmail.com>
2024-06-24 11:41:08 -0600

    feat(Notes): implemented bootstrap classes on templates

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NoteListView.as_view()),
-    path('notes/<int:pk>', views.NoteDetailView.as_view()),
+    path('notes', views.NoteListView.as_view(), name='note.list'),
+    path('notes/<int:pk>', views.NoteDetailView.as_view(), name='note.detail'),
 ]
