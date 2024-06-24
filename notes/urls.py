db783ffab196b30f7668fb268a5ade78142b851b
Jared García <ljared.31g@gmail.com>
2024-06-24 11:57:12 -0600

    feat(Notes): implemented creation form view

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('notes', views.NoteListView.as_view(), name='note.list'),
     path('notes/<int:pk>', views.NoteDetailView.as_view(), name='note.detail'),
+    path('notes/new', views.NoteCreateView.as_view(), name='note.new'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 from .models import Note
 
 
 # When using class-based views such as ListView or DetailView,
 # we need to comply with the naming conventions of the templates.
 # Otherwise, the name of the template is specified in the property 'template_name'
+class NoteCreateView(CreateView):
+    model = Note
+    fields = ['title', 'text']
+    success_url = '/smart/notes'
+
+
 class NoteListView(ListView):
     model = Note
     context_object_name = 'notes'
