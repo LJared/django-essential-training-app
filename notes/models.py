b5ded8030911ae3f71c05aca3b5570ea5a9e540e
Jared García <ljared.31g@gmail.com>
2024-07-02 09:07:15 -0600

    feat(Note): added user_id foreign key

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='note_ids')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def concat_title_and_text(self):        
